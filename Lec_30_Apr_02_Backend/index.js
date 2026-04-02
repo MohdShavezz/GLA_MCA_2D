24b91605387a21af54076e6ef09b4e13266ef961
MohdShavezz <mohdpctebtech19cse@gmail.com>
2026-04-02 14:45:37 +0530

    mongoose express: crud apis for products db config MVC framwork

---
+++ Lec_30_Apr_02_Backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import productRouter from './routes/product.js'
+import dbConn from './db/dbConn.js';
+
+dotenv.config({})
+//db connect
+dbConn()
+
+const app = express()
+app.use(express.json())
+
+app.use('/api',productRouter)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('server is running on ', PORT)
+})
